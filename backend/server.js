2e0048d1e643b5b1f6153693bfc9f5274ed7707f
vivek <vivekbk1997@gmail.com>
2022-04-21 13:38:10 +0530

    fixed outer package

--- backend/server.js
+++ backend/server.js
@@ -17,12 +17,10 @@ const app = express()
 const _dirname1 = path.resolve()
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(_dirname1, '..', '/frontend/build')))
+  app.use(express.static(path.join(_dirname1, '/frontend/build')))
 
   app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(_dirname1, '..', 'frontend', 'build', 'index.html')
-    )
+    res.sendFile(path.resolve(_dirname1, 'frontend', 'build', 'index.html'))
   )
 } else {
   app.get('/', (req, res) => {
